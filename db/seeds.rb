014dc087b4aaa66a24c228ad52690f7f00383ef5
Melanie Schmitz-Jansen <melanieschmitzj@gmail.com>
2019-02-13 17:25:23 +0100

    restaurant with reviews not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  @restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: CATEGORY.sample
+    )
+  @restaurant.save!
+end
+
+puts 'Finished!'
+
+
+
+C
